ff9f9207978fd9a49b9cd8b4b96e95de1c4404bb
Xinlong She <19816542@student.westernsydney.edu.au>
2021-12-24 13:08:54 +1100

    uncomplete controller and models

---
+++ Models/Trainer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Test_4._0.Models
+{
+    public class Trainer
+    {
+        public int TrainerID { get; set; }
+        public string TrainerName { get; set; }
+        public string TrainerPassword { get; set; }
+    }
+}
