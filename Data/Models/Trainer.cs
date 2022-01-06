d65ccf3665a63a67d4fae825740aae8aadfb870e
Wasee-ur Rahman <19267904@student.westernsydney.edu.au>
2022-01-07 09:50:41 +1100

    work

---
+++ Data/Models/Trainer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Test_4._0.Data.Models
+{
+    public class Trainer
+    {
+
+    }
+}
